91a2a7f550a5e71f1222377bf3dc503c44dd059c
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-03-30 12:04:30 +0200

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,29 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double differenceAndDivide(double first, double second) {
+        return difference(first, second)
+                + divide(first, second);
+    }
+
+    public static double sumOfAllFunction(double first, double second) {
+        return difference(first, second)
+                + divide(first, second)
+                + sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+
+
+}
