4cda67fabbf3b69ba4274a3e64abd1cdd94f7517
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-04-07 22:54:40 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/chapter1/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/chapter1/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Vlad";
+        names[2] = "Stas";
+        names[3] = "Ruslan";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
