18580a7384cb3ecbde3201ec08bbf2fd2b499a9e
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-04-21 19:23:54 +0300

    3.2. Удаление моделей из массива. [#310016]

---
+++ src/main/java/ru/job4j/chapter2/encapsulation/ShopDrop.java
@@ -0,0 +1,16 @@
+package ru.job4j.chapter2.encapsulation;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        if (index >= products.length) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = index; i < products.length; i++) {
+            if(i + 1 < products.length) {
+                products[i] = products[i + 1];
+            }
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
