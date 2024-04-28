ecf3737e670c6702d40b19773805987fdc7da3a0
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-04-28 12:03:37 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/chapter2/exception/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.chapter2.exception;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if(value[i] != null && key.equals(value[i])) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("element non found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] example = new String[] {"test1", "test2", "test3"};
+        try {
+            int result = indexOf(example, "test4");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
