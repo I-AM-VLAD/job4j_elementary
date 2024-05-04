0075a1f4261e677e2c9ebce5f6a33f418e6a84e4
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-05-04 09:30:56 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #494136]

--- src/main/java/ru/job4j/chapter3/list/PhoneDictionary.java
+++ src/main/java/ru/job4j/chapter3/list/PhoneDictionary.java
@@ -18,9 +18,9 @@ public class PhoneDictionary {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (person.getAddress().contains(key)
-            || person.getName().contains(key)
+                    || person.getName().contains(key)
                     || person.getPhone().contains(key)
-            || person.getSurname().contains(key)) {
+                    || person.getSurname().contains(key)) {
                 result.add(person);
             }
         }
