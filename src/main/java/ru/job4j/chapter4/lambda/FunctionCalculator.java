721af7d3014034be51fae9044bc978156ab7d196
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-05-08 14:05:44 +0300

    6. Подсчет функции в диапазоне. [#24251]

--- src/main/java/ru/job4j/chapter4/lambda/FunctionCalculator.java
+++ src/main/java/ru/job4j/chapter4/lambda/FunctionCalculator.java
@@ -8,7 +8,7 @@ public class FunctionCalculator {
     public List<Double> diapason(int start, int end, Function<Double, Double> function) {
         List<Double> result = new ArrayList<>();
         for (int i = start; i < end; i++) {
-            result.add(function.apply((double)i));
+            result.add(function.apply((double) i));
         }
         return result;
     }
